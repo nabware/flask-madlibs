8b3b66b08855334cee3afda19de2c5611ff7f125
Nabeel Rana <nabwaregroup@gmail.com>
2023-09-27 15:33:01 -0700

    Added input fields

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/")
+def home():
+    """Returns home page with story form"""
+
+    return render_template("questions.html", prompts=silly_story.prompts)
