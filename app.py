c3caace5d1c23d07c4c29dfb55591b36fcd331a2
JaeWon Jin <j1jin286@gmail.com>
2023-09-27 16:03:26 -0700

    Code review complete

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import silly_story, excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -13,3 +13,10 @@ def home():
     """Returns home page with story form"""
 
     return render_template("questions.html", prompts=silly_story.prompts)
+
+@app.get("/results")
+def results():
+    """Returns results page with filled story"""
+
+    return render_template("results.html",
+                           story=silly_story.get_result_text(request.args))
