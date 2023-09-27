5cfb1d89f9dc8b09dffd0586e612b624e4ee4eb5
Nabeel Rana <nabwaregroup@gmail.com>
2023-09-27 16:26:56 -0700

    Added user picked story functionality

--- app.py
+++ app.py
@@ -8,15 +8,28 @@ app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+stories = {"silly story": silly_story,
+           "excited story": excited_story
+        }
+
 @app.get("/")
 def home():
-    """Returns home page with story form"""
+    """Returns home page with dropdown to select story"""
 
-    return render_template("questions.html", prompts=silly_story.prompts)
+    return render_template("stories.html", stories=stories.keys())
 
-@app.get("/results")
-def results():
+@app.get("/questions")
+def questions():
+    """Returns questions page with story form"""
+
+    story_key = request.args.get("story")
+    story = stories.get(story_key)
+
+    return render_template("questions.html", prompts=story.prompts, story=story_key)
+
+@app.get("/<story>/results")
+def results(story):
     """Returns results page with filled story"""
 
     return render_template("results.html",
-                           story=silly_story.get_result_text(request.args))
+                           story=stories[story].get_result_text(request.args))
