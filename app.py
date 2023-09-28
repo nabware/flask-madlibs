9d466b43551b95983e9056dea71d780188f1e219
JaeWon Jin <j1jin286@gmail.com>
2023-09-27 17:07:40 -0700

    Completed further study

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_debugtoolbar import DebugToolbarExtension
+import re
 
-from stories import silly_story, excited_story
+from stories import Story, silly_story, excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -33,3 +34,22 @@ def results(story):
 
     return render_template("results.html",
                            story=stories[story].get_result_text(request.args))
+
+@app.get("/create")
+def show_story_template_form():
+    """Show from to create a new story template"""
+
+    return render_template("create_story_template.html")
+
+@app.post("/create")
+def create_story_template():
+    """Create a new story template and include in local stories list"""
+
+    story_name = request.form.get("story_name")
+    story_template = request.form.get("story_template")
+    parts_of_speech = set(re.findall(r'\{([^}]+)\}', story_template))
+
+    stories[story_name] = Story(parts_of_speech, story_template)
+
+    return redirect("/")
+
